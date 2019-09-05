a0f286822699f5a82712633b5c4682e6dfb9c3f4
jaboud <jaboud94@gmail.com>
2019-09-05 14:35:57 +1000

    Initial solution creation

---
+++ HackerRank-Solve-Me-First/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Solve_Me_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
