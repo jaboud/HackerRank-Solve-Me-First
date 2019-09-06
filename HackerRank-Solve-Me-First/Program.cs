af084ffe0f6e28fe571871b604034e242dbcf94b
jaboud <jaboud94@gmail.com>
2019-09-06 10:05:30 +1000

    Static variable delcaration

--- HackerRank-Solve-Me-First/Program.cs
+++ HackerRank-Solve-Me-First/Program.cs
@@ -4,6 +4,10 @@ namespace HackerRank_Solve_Me_First
 {
     class Program
     {
+        static int solveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
