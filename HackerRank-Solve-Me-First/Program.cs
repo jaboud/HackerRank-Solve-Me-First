d3792a8c12bb0748102257f3d973e883ab495f99
jaboud <jaboud94@gmail.com>
2019-09-09 11:56:40 +1000

    Comments for first and second integer declaration

--- HackerRank-Solve-Me-First/Program.cs
+++ HackerRank-Solve-Me-First/Program.cs
@@ -10,7 +10,9 @@ namespace HackerRank_Solve_Me_First
         }
         static void Main(string[] args)
         {
+            //First integer value declaration and converted to Int32 type.
             int val1 = Convert.ToInt32(Console.ReadLine());
+            //Second integer value declaration and converted to Int32 type.
             int val2 = Convert.ToInt32(Console.ReadLine());
             int sum = SolveMeFirst(val1, val2);
             Console.WriteLine(sum);
